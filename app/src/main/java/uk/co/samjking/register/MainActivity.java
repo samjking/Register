8e75aa0be3d1daaa06e3971d0561b9eae6834f8d
samjking <sam@samjking.co.uk>
2015-08-13 15:57:42 +0100

    Added photos

--- app/src/main/java/uk/co/samjking/register/MainActivity.java
+++ app/src/main/java/uk/co/samjking/register/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView iv = (ImageView)findViewById(R.id.photo1);
+        iv.setImageResource(R.drawable.a380002);
     }
 
     @Override
