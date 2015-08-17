870af81f3bb0e0036728dcb6dbdcb6566303e725
samjking <sam@samjking.co.uk>
2015-08-17 22:09:59 +0100

    Basic working carousel

--- app/src/main/java/uk/co/samjking/register/MainActivity.java
+++ app/src/main/java/uk/co/samjking/register/MainActivity.java
@@ -1,19 +1,57 @@
 package uk.co.samjking.register;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Define the number of items visible when the carousel is first shown.
+     */
+    private static final float INITIAL_ITEMS_COUNT = 1.5F;
+
+    private LinearLayout mCarouselContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView iv = (ImageView)findViewById(R.id.photo1);
-        iv.setImageResource(R.drawable.a380002);
+
+        mCarouselContainer = (LinearLayout) findViewById(R.id.carousel);
+
+        // Compute the width of a carousel item based on the screen width and number of initial items.
+        final DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        final int imageWidth = (int) (displayMetrics.widthPixels / INITIAL_ITEMS_COUNT);
+
+        final TypedArray photoResourcesTypedArray = getResources().obtainTypedArray(R.array.photo_array);
+
+
+        // Populate the carousel with items
+        ImageView imageItem;
+        for (int i = 0 ; i < photoResourcesTypedArray.length() ; ++i) {
+            // Create new ImageView
+            imageItem = new ImageView(this);
+
+            // Set the shadow background
+            //imageItem.setBackgroundResource(R.drawable.shadow);
+
+            // Set the image view resource
+            imageItem.setImageResource(photoResourcesTypedArray.getResourceId(i, -1));
+
+            // Set the size of the image view to the previously computed value
+            imageItem.setLayoutParams(new LinearLayout.LayoutParams(imageWidth, imageWidth));
+
+            /// Add image view to the carousel container
+             mCarouselContainer.addView(imageItem);
+        }
+
     }
 
     @Override
